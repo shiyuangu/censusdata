c384f9b1600a2d763b826b6e0488173c251db5f9
shgu <shgu@adobe.com>
2024-06-20 14:06:19 -0700

    check in all changes during mac migration. The changes are not verified.

--- censusdata/__init__.py
+++ censusdata/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .censusgeo import *
-from .variable_info import *
+from .variable_info2 import *
 from .download import *
 from .download import _download
 from .export import *
